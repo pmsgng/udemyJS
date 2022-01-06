351844e26e576bf84ba2bbff1c359be02ecc1583
Владислав Цой <pms@MacBook-Air-Vladislav.local>
2022-01-06 17:43:43 +0300

    совместное применение циклов и условий

--- script.js
+++ script.js
@@ -10,70 +10,96 @@ const personalMovieDB = {
     privat: false,
 };
 
-const a = prompt('Один из последних просмотренных фильмов?' , '');
-const b = prompt('На сколько оцените его?' , '');
-const c = prompt('Один из последних просмотренных фильмов?' , '');
-const d = prompt('На сколько оцените его?' , '');
+// const a = prompt('Один из последних просмотренных фильмов?' , '');
+// const b = prompt('На сколько оцените его?' , '');
+// const c = prompt('Один из последних просмотренных фильмов?' , '');
+// const d = prompt('На сколько оцените его?' , '');
 
-personalMovieDB.movies[a] = b;
-personalMovieDB.movies[c] = d;
+// personalMovieDB.movies[a] = b;
+// personalMovieDB.movies[c] = d;
+
+for (let s = 0; s < 2; s++) {
+    const a = prompt('Один из последних просмотренных фильмов?', '');
+    const b = prompt('На сколько оцените его?', '');
+
+    if ( a != null && b != null && a != '' && b != '' && a.length < 50 ) {
+        personalMovieDB.movies[a] = b;
+        console.log('done');
+    } else {
+        console.log('error');
+        s--;
+    }
+}
+
+if (personalMovieDB.count < 10) {
+    console.log('просмотрено довольно много фмльмов');
+} else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
+    console.log('вы классический зритель');
+} else if (personalMovieDB.count >= 30) {
+    console.log('вы киноман!');
+} else {
+    console.log('Произошла ошибка!');
+}
 
 console.log(personalMovieDB);
 
-if (1) {
-    console.log('Ok!');
-} else {
-    console.log('Error!');
-}
 
-const num = 50;
 
-if (num < 49) {
-    console.log('Error!');
-} else if (num > 100) {
-    console.log('Слишком много');
-} else {
-    console.log('Ok!');
-}
+//-------------------------------------
+// if (1) {
+//     console.log('Ok!');
+// } else {
+//     console.log('Error!');
+// }
 
-(num == 50) ? console.log('Ok!') : console.log('Error!');
+// const num = 50;
 
-switch (num) {
-    case 49:
-        console.log('Неверно!');
-        break;
-    case 100:
-        console.log('Неверно!');
-        break;
-    case 50:
-        console.log('Ok!');
-        break;
-    default:                                        //прописваем если нет верного ответа
-        console.log('Нет праивльного ответа');
-        break;
-}
+// if (num < 49) {
+//     console.log('Error!');
+// } else if (num > 100) {
+//     console.log('Слишком много');
+// } else {
+//     console.log('Ok!');
+// }
 
-let number = 50;
+// (num == 50) ? console.log('Ok!') : console.log('Error!');
 
-while (number < 55) {
-    console.log(number);
-    number++;
-} 
+// switch (num) {
+//     case 49:
+//         console.log('Неверно!');
+//         break;
+//     case 100:
+//         console.log('Неверно!');
+//         break;
+//     case 50:
+//         console.log('Ok!');
+//         break;
+//     default:                                        //прописваем если нет верного ответа
+//         console.log('Нет праивльного ответа');
+//         break;
+// }
 
-do {
-    console.log(number);
-    number++;
-} while (number < 55);
+// let number = 50;
 
-for (let i = 1; i < 8; i++) {
-    console.log(number);
-    number++;
-}
+// while (number < 55) {
+//     console.log(number);
+//     number++;
+// } 
 
-for (let t = 1; t < 10; t++) {
-    if(t === 6) {
-        break;
-        // continue;      оператор не прерывает цикл,а пропускает число 6 в цикле
-    }
-    console.log(t);
-}
+// do {
+//     console.log(number);
+//     number++;
+// } while (number < 55);
+
+// for (let i = 1; i < 8; i++) {
+//     console.log(number);
+//     number++;
+// }
+
+// for (let t = 1; t < 10; t++) {
+//     if(t === 6) {
+//         break;
+//         // continue;      оператор не прерывает цикл,а пропускает число 6 в цикле
+//     }
+//     console.log(t);
+// }
