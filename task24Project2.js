d268847978a05686e7e844f24c3670eefdd6ca08
Владислав Цой <pms@MacBook-Air-Vladislav.local>
2022-01-10 23:26:50 +0300

    Отлавливаем ошибки в коде через консоль разработчика. Breakpoints.

--- task24Project2.js
+++ task24Project2.js
@@ -6,14 +6,14 @@ const personalMovieDB = {
     actors: {},
     genres: [],
     privat: false,
-    start: function() {
+    start: function () {
         personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
 
         while (personalMovieDB.count == '' || personalMovieDB.count == null || isNaN(personalMovieDB.count)) {
             personalMovieDB.count = prompt('Сколько фильмов вы уже посмотрели?', '');
         }
     },
-    rememberMyFilms: function() {
+    rememberMyFilms: function () {
         for (let s = 0; s < 2; s++) {
             const a = prompt('Один из последних просмотренных фильмов?', '');
             const b = prompt('На сколько оцените его?', '');
@@ -27,7 +27,7 @@ const personalMovieDB = {
             }
         }
     },
-    detectPersonalLevel: function() {
+    detectPersonalLevel: function () {
         if (personalMovieDB.count < 10) {
             console.log('просмотрено довольно много фмльмов');
         } else if (personalMovieDB.count >= 10 && personalMovieDB.count < 30) {
@@ -38,31 +38,40 @@ const personalMovieDB = {
             console.log('Произошла ошибка!');
         }
     },
-    showMyDB: function(hidden) {
-        if(!hidden) {
+    showMyDB: function (hidden) {
+        if (!hidden) {
             console.log(personalMovieDB);
         }
     },
-    toggleVisibleMyDB: function() {
+    toggleVisibleMyDB: function () {
         if (personalMovieDB.privat) {
             personalMovieDB.privat = false;
         } else {
             personalMovieDB.privat = true;
         }
     },
-    writeYourGenres: function() {
-        for (let i = 1; i <= 3; i++) {
-            let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
+    writeYourGenres: function () {
+        // for (let i = 1; i <= 3; i++) {
+            // let genre = prompt(`Ваш любимый жанр под номером ${i}`).toLowerCase();
 
-            if( genre === '' || genre == null) {
+            // if( genre === '' || genre == null) {
+            //     console.log("вы ввели некорректные данные или не ввели их вообще");
+            //     i--;
+            // } else {
+            //     personalMovieDB.genres[i - 1] = genre;
+            // }
+        for (let i = 1; i < 2; i++) { 
+            let genre = prompt(`Введите Ваш любимый жанр через запятую!`).toLowerCase();
+
+            if (genre === '' || genre == null) {
                 console.log("вы ввели некорректные данные или не ввели их вообще");
                 i--;
             } else {
-                personalMovieDB.genres[i - 1] = genre;
+                personalMovieDB.genres = genre.split(', ').sort();
             }
         }
         personalMovieDB.genres.forEach((item, i) => {
             console.log(`Любимый жанр ${i + 1} - это ${item}`);
-        })
+        });
     },
 };
