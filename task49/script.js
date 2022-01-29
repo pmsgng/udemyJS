a0ec100d7666d3ce65dde47a8ce4c06966355c02
Tsoy Vlad <pmsgng88@gmail.com>
2022-01-29 20:41:22 +0300

    AJAX и общение с сервером

--- task49/script.js
+++ task49/script.js
@@ -6,8 +6,8 @@ const log = function(a,b, ...rest) {
 
 log('basic','rest','operator','usage');
 
-function calcOrDuble(number, basis) {
-    basis = basis || 2;
+function calcOrDuble(number, basis = 2) {  // значение по умолчанию basis = 2, если не передан аргумент при вызове функции
+    // basis = basis || 2;
     console.log(number * basis);
 };
 
