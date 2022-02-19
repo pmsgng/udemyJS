7c2dba6e84bc07e1607be3240a135e481996e0d1
Tsoy Vlad <pmsgng88@gmail.com>
2022-02-19 19:54:00 +0300

    Вспоминаем функции конструкторы и классы

--- task45/script.js
+++ task45/script.js
@@ -2,25 +2,45 @@
 
 // стандарты ES5 
 
-function User(name, id) {
-    this.name = name;
-    this.id = id;
-    this.human = true;
-    this.hello = function() {
-        console.log(`Hello ${this.name}`);
-    };
+// function User(name, id, age) {
+//     this.name = name;
+//     this.id = id;
+//     this.human = true;
+//     this.age = age;
+//     this.hello = function() {
+//         console.log(`Hello ${this.name}`);
+//     };
+// }
+
+// User.prototype.exit = function() {
+//     console.log(`Пользователь ${this.name + ' ' + this.id} ушёл`);
+// };
+
+// const ivan = new User('Ivan','20');
+// const alex = new User('Alex','15');
+// const dima = new User('DIma','10', '25')
+
+// alex.hello();
+// ivan.hello();
+// ivan.exit();
+
+// console.log(ivan,alex, dima);
+
+class User2 {
+    constructor(name,id) {
+        this.name = name;
+        this.id = id;
+    }
+    hello() {
+        console.log(`Hello ${this.name + ' ' + this.id}`)
+    }
+    exit() {
+        console.log(`User ${this.name} ${this.id} exit from server`)
+    }
 }
 
-User.prototype.exit = function() {
-    console.log(`пользователь ${this.name} ушёл`);
-};
+const petr = new User2('Petr','25');
 
-const ivan = new User('Ivan','20');
-const alex = new User('Alex','15');
-
-ivan.exit();
-
-console.log(ivan,alex);
-
-ivan.hello();
-alex.hello();
+console.log(petr);
+petr.exit();
+petr.hello();
